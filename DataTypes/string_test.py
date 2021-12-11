2d67d94a4f1bc656fc7ab5a573383c2106037d1a
Vamsi <vamsi.thotlapalepu@gmail.com>
2021-12-12 00:24:11 +0900

    strings

---
+++ DataTypes/string_test.py
@@ -0,0 +1,17 @@
+def main():
+    a  = "Delhi is the capital of India"
+
+    b = "Again"
+
+    x = "1"
+    y = "2"
+    z =  x + y
+
+
+    print(z, type(z))
+
+
+    c = b[0:-2]
+    print(c)
+
+main()
