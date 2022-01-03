e2870dcb8ec884fd770184926a503523a41a89a8
Vamsi <vamsi.thotlapalepu@gmail.com>
2022-01-03 20:33:03 +0900

    functions

---
+++ DataTypes/eliif_demo.py
@@ -0,0 +1,14 @@
+# given a number = positive or negative or zero
+# expr of positive = a > 0
+# expr of negative = a < 0
+# expr of zero = a == 0
+
+a = -45
+
+if a > 0:
+    print("Positive Number")
+elif a < 0:
+    print("Negative Number")
+else:
+    print("Number is Zero")
+
